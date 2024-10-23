aef3e1ff45586a498a3c1f4a08cb46ed7bffef3e
KimDongHa00 <dhk8110@gmail.com>
2024-10-23 16:26:10 +0900

    Update download.sh
    .

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,20 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
+# Navigate to the parent directory
 cd $SCRIPTPATH/..
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
-unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
+
+# Define the Hugging Face URL for the dataset
+DATASET_URL="https://huggingface.co/datasets/ShapeNet/ShapeNetCore/resolve/main/02828884.zip"  # Example for 'bench'
+
+# Download the dataset
+wget $DATASET_URL --no-check-certificate -O "shapenetcore_partanno_segmentation_benchmark_v0.zip"
+
+# Unzip the downloaded file
+unzip shapenetcore_partanno_segmentation_benchmark_v0.zip -d ./shapenet_data
+
+# Remove the zip file after unzipping
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
+
+# Return to the previous directory
 cd -
